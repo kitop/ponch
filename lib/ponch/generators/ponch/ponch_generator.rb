4bef1af443a5ad55ade188cad5a02edf610e3ab9
Esteban Pastorino <ejpastorino@gmail.com>
2011-11-30 18:37:16 -0300

    try migration generator

--- lib/ponch/generators/ponch/ponch_generator.rb
+++ lib/ponch/generators/ponch/ponch_generator.rb
@@ -1,15 +1,23 @@
 require 'rails/generators'
-require "rails/generators/active_record"
+require "rails/generators/migration"
 
 module Ponch
   module Generators
 
-    class PonchGenerator < Rails::Generators::NamedBase
+    class PonchGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
 
-      def manifest
-        record do |m|
-          m.migration_template('create_deliveries.rb', 'db/migrate', :migration_file_name => 'create_ponch_deliveries')
+      source_root File.expand_path('../templates', __FILE__)
+
+      def copy_files
+        migration_template('create_deliveries.rb', 'db/migrate/create_ponch_deliveries.rb')
+      end
+
+      def self.next_migration_number(dirname) #:nodoc:
+        if ActiveRecord::Base.timestamped_migrations
+          Time.now.utc.strftime("%Y%m%d%H%M%S")
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
         end
       end
 
